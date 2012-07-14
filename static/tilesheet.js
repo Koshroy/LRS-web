f8a464d97a54d293db4f654832fa92c4291199b5
Koushik Roy <koshroy@Koushik-Roys-MacBook-Pro.local>
2012-07-14 18:23:16 -0500

    Added map and tilesheet helper classes

---
+++ static/tilesheet.js
@@ -0,0 +1,43 @@
+function TileSheet(canvas, sheetImg, tile_w, tile_h)
+{
+    this.canvas = canvas[0];
+    this.sheet = sheetImg;
+    this.tile_w = tile_w;
+    this.tile_h = tile_h;
+
+    this.horizTiles = this.sheet.width() / this.tile_w;
+    this.vertTiles = this.sheet.height() / this.tile_h;
+
+    
+    this.writeTile = function(tileNum, tx, ty)
+    {
+	var tile_x = tileNum % horizTiles;
+	var tile_y = tileNum / horizTiles;
+	this.canvas.drawImage(this.sheet, tile_x, tile_y, this.tile_w, this.tile_h, tx, ty, this.tile_w, this.tile_h);
+    }
+}
+
+function Map(sizeX, sizeY, tile_w, tile_h)
+{
+    this.size_x = sizeX;
+    this.size_y = sizeY;
+
+    this.tile_w = tile_w;
+    this.tile_h = tile_h;
+    
+    this.arr = new Array(sizeX*sizeY);
+
+    this.drawTileOnMap = function(sheet, mx, my)
+    {
+	// Off bounds of map
+	if ( (mx >= this.size_x) || (my >= this.size_y) )
+	{
+	    console.log("Bad dimensions!");
+	    return;
+	}
+
+	var tnum = this.arr[my*this.size_x + mx];
+
+	sheet.writeTile(tnum, mx*tile_x, my*tile_y);
+    }
+}
